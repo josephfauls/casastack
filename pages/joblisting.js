bcbe6b63d7d5924065c3353e393ef5274f2ffc6a
Lucas Liseth <lucasl2000@gmail.com>
2020-07-24 16:14:49 -0500

    Listings now display in a row (instead of vertical)

--- pages/joblisting.js
+++ pages/joblisting.js
@@ -4,7 +4,7 @@ import { Button, Card, Segment } from 'semantic-ui-react';
 import listingStyle from '../components/joblistingsPage/jobListingPageStyles/joblisting.module.css';
 
 const joblistings1 = ({ listings }) => {
-    return(
+    return (
         <div className={listingStyle.listingLayout}>
             <h1>Listings</h1>
             <Button circular size='big' color='twitter' type='submit'>
@@ -12,63 +12,47 @@ const joblistings1 = ({ listings }) => {
                     <a className="create">Create Listing</a>
                 </Link>
             </Button>
-        <Segment className={listingStyle.listingLayout}>
-            {listings.map(listing => {
-                return (
-                    <div key={listing._id}>
+            <Card.Group itemsPerRow={3}>
+                {listings.map(listing => {
+                    return (
                         <Card centered columnCount="3">
                             <Card.Content>
                                 <Card.Header>
-
                                     <Link href={`/${listing._id}`}>
                                         <h1>{listing.service}</h1>
                                     </Link>
-                                    
                                 </Card.Header>
                             </Card.Content>
-
                             <Card.Content extra>
-
-                                
-                                    <div>
-                                        <Button primary size="small">
-                                            <Link href={`/${listing._id}`}>
-                                                <h3>View Listing</h3>
-                                            </Link>
-                                        </Button>
-                                    
-
-
-                                
-                                    
-                                        <Button primary size="small">
-                                            <Link href={`/${listing._id}/edit`}>
-                                                <h3>Edit Listing</h3>
-                                            </Link>
-                                        </Button>
-                                    </div>
-                                    
-                                    
-                            
-
+                                <div>
+                                    <Button primary size="small">
+                                        <Link href={`/${listing._id}`}>
+                                            <h3>View Listing</h3>
+                                        </Link>
+                                    </Button>
+                                    <Button primary size="small">
+                                        <Link href={`/${listing._id}/edit`}>
+                                            <h3>Edit Listing</h3>
+                                        </Link>
+                                    </Button>
+                                </div>
                             </Card.Content>
                         </Card>
-                    </div>
-                )
-            })}
-        
-            
-        </Segment>
+                        /* <div key={listing._id}> */
+                        /* </div> */
+                    );
+                })}
+
+            </Card.Group>
         </div>
-    )
-    
-}
+    );
+};
 
 joblistings1.getInitialProps = async () => {
     const res = await fetch('http://localhost:3000/api/listings');
     const { data } = await res.json();
 
-    return { listings: data }
-}
+    return { listings: data };
+};
 
 export default joblistings1;
